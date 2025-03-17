2bbd937a263d4e4cf7b1bb883be818198d13bc44
GabrielC67 <gabecruzpro@gmail.com>
2025-03-17 17:06:29 -0400

    Checkpoint

--- src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
@@ -1,7 +1,55 @@
 package com.github.zipcodewilmington.casino.games.slots;
+import java.util.Scanner;
+
+
+import java.util.Random;
+
+
 
 /**
  * Created by leon on 7/21/2020.
  */
 public class SlotsGame {
+/*
+    columnone runs, then stops at a number or object, then columntwo runs, and so on...
+    Once all three stop, then they are compared to find out if they are matching..
+    If all three match, then the player wins double their winnings. player's account + (wager * 2).
+
+ */
+    int slot1 = 0;
+    int slot2 = 0;
+    int slot3 = 0;
+
+
+private double getUserInput(String s) {
+    System.out.println(s);
+    Scanner bet = new Scanner(System.in);
+    return bet.nextDouble();
+}
+
+ public void bet(double balance){
+     System.out.println("Let's Play!!");
+     double amountToBet = getUserInput("How much would you like to bet?");
+     pullTrigger();
+     System.out.println(slot1);
+     System.out.println(slot2);
+     System.out.println(slot3);
+ }
+
+    private void pullTrigger() {
+        slot1 = randomColumn();
+        slot2 = randomColumn();
+        slot3 = randomColumn();
+    }
+
+    public int randomColumn() {
+    Random num = new Random();
+    return num.nextInt(7);
+    }
+
+    public static void main(String[] args) {
+        SlotsGame sg = new SlotsGame();
+        sg.bet(1000.00);
+    }
+
 }
--- src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsPlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsPlayer.java
@@ -4,4 +4,14 @@ package com.github.zipcodewilmington.casino.games.slots;
  * Created by leon on 7/21/2020.
  */
 public class SlotsPlayer {
+    /*
+    About the player: Find how much the player has remaining after playing other games.
+
+
+    Find out if the user has enough money to play slots.
+    Then add the bet (wager). Whatever money they have minus the wager.
+    "Are you ready? Pull the trigger to begin!!!" --> SlotsGame class becomes active
+    */
+
+
 }
