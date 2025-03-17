2589b8c9c229dbed4cdceedb712f33eea63e5fd3
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-03-17 16:07:07 -0400

    rough outlineish

--- src/main/java/com/github/zipcodewilmington/casino/games/blackjack/BlackjackGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/blackjack/BlackjackGame.java
@@ -1,4 +1,28 @@
 package com.github.zipcodewilmington.casino.games.blackjack;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class BlackjackGame {
+     ArrayList<String>  dealerHand;
+     ArrayList<String>  playerHand;
+    Scanner input = new Scanner(System.in);
+    Random random = new Random();
+   Object[] deck ;
+
+ public void playBlackJack(){
+     deck = new Object[] {2,3,4,5,6,7,8,9,10,"J","Q","K","A"};
+     intro();
+
+ }
+
+
+    private void intro() {
+        System.out.println("Welcome to BlackJack");
+
+    }
+    public  Object getDeck(){
+     return deck;
+    }
 }
