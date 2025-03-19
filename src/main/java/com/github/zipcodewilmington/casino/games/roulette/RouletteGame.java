3f3735ac870d521f8aec5a7a036f4fbe1c6ea725
Nathan <natefintak@gmail.com>
2025-03-19 15:08:41 -0400

    in progress

--- src/main/java/com/github/zipcodewilmington/casino/games/roulette/RouletteGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/roulette/RouletteGame.java
@@ -3,14 +3,14 @@ import java.util.Random;
 
 
 public class RouletteGame {
-    Random rand = new Random();
+    private Random rand = new Random();
     //var from superclass casino needed? - casinoWallet
-    int currentNum;
-    String currentColor;
-    String oddOrEven;
-    double currentBet;
-    boolean playGame = true;
-    String[][] rouletteTable = {{"  ", "|    ", "   1-", "-12  ", "     ", "|    ", "  13-", "-24  ", "     ", "|    ", "  25-", "-36  \n"},
+    private int currentNum;
+    private String currentColor;
+    private String oddOrEven;
+    private double currentBet;
+    private boolean playGame = true;
+    private String[][] rouletteTable = {{"  ", "|    ", "   1-", "-12  ", "     ", "|    ", "  13-", "-24  ", "     ", "|    ", "  25-", "-36  \n"},
             {"  ", "|  3 ", "|  6 ", "|  9 ", "| 12 ", "| 15 ", "| 18 ", "| 21 ", "| 24 ", "| 27 ", "| 30 ", "| 33 ", "| 36 ", "|2 to 1\n"},
             {" 0", "|  2 ", "|  5 ", "|  8 ", "| 11 ", "| 14 ", "| 17 ", "| 20 ", "| 23 ", "| 26 ", "| 29 ", "| 32 ", "| 35 ", "|2 to 1\n"},
             {"  ", "|  1 ", "|  4 ", "|  7 ", "| 10 ", "| 13 ", "| 16 ", "| 19 ", "| 22 ", "| 25 ", "| 28 ", "| 31 ", "| 34 ", "|2 to 1\n"},
@@ -19,7 +19,7 @@ public class RouletteGame {
     public RouletteGame() {
     }
 
-    public void runGame() {
+    private void runGame() {
         //holds order of methods when game is running
 //        while (playGame) {
 //
@@ -38,7 +38,7 @@ public class RouletteGame {
         //add 2d array of the board, hardcode it in?
     }
     private void spinWheel() {
-        //this.currentNum = rand.nextInt(0, 37); //may need to set origin to -1, will test to be sure
+        this.currentNum = rand.nextInt(0, 37); //may need to set origin to -1, will test to be sure
     }
     private int getCurrentNum() {
         return this.currentNum;
