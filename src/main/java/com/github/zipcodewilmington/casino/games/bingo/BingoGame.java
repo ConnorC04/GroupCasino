1843c23a358b31a4ed913613262e2c7d8847d390
ConnorC04 <connorjcasey@iCloud.com>
2025-03-19 14:40:48 -0400

    testing git

--- src/main/java/com/github/zipcodewilmington/casino/games/bingo/BingoGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/bingo/BingoGame.java
@@ -1,17 +1,53 @@
 package com.github.zipcodewilmington.casino.games.bingo;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class BingoGame {
 
+    private Random random = new Random();
     private int numberChosen;
+    private ArrayList<Integer> numbers;
 
     public BingoGame(){
     }
 
+    public ArrayList<Integer> setNumbers(ArrayList<Integer> num, int start, int end){
+        for (int i = start; i < end; i++){
+             num.add(i);
+        }
+        return num;
+    }
+
     public int[][] makeBoard(){
+
+        ArrayList<Integer> firstColumn = new ArrayList<>();
+        ArrayList<Integer> secondColumn = new ArrayList<>();
+        ArrayList<Integer> thirdColumn = new ArrayList<>();
+        ArrayList<Integer> fourthColumn = new ArrayList<>();
+        ArrayList<Integer> fifthColumn = new ArrayList<>();
+
+        BingoGame bingoGame = new BingoGame();
+
+        bingoGame.setNumbers(firstColumn, 1, 15);
+        bingoGame.setNumbers(secondColumn, 16, 30);
+
+        int[][] board = new int[5][5];
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j < 5; j++){
+                if (i == 0){
+
+                }
+            }
+        }
         return null;
     }
 
-    public int chooseRandomNumber(){
+    public int chooseRandomNumber(ArrayList<Integer> num){
+
+//        int randomChoice = random.nextInt(1, 75);
+//        num.remove(randomChoice);
+//        return randomChoice;
         return 0;
     }
 
--- src/test/java/com/github/zipcodewilmington/BingoTest.java
+++ src/test/java/com/github/zipcodewilmington/BingoTest.java
@@ -1,4 +1,29 @@
 package com.github.zipcodewilmington;
 
+import com.github.zipcodewilmington.casino.games.bingo.BingoGame;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class BingoTest {
+
+    @Test
+    public void testResetNumbers(){
+        ArrayList<Integer> expected = new ArrayList<>();
+        for (int i = 1; i < 75; i++){
+            expected.add(i);
+        }
+        BingoGame bingo = new BingoGame();
+        ArrayList<Integer> actual = new ArrayList<>();
+        bingo.setNumbers(actual, 1, 75);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRandomChoice(){
+
+    }
 }
