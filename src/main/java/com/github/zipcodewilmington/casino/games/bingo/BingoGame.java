a9e0c87fd87d13aef3a0ac7e9f4965106c02de95
ConnorC04 <connorjcasey@iCloud.com>
2025-03-19 10:08:40 -0400

    added bingo methods, might need more

--- src/main/java/com/github/zipcodewilmington/casino/games/bingo/BingoGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/bingo/BingoGame.java
@@ -1,4 +1,37 @@
 package com.github.zipcodewilmington.casino.games.bingo;
 
 public class BingoGame {
+
+    private int numberChosen;
+
+    public BingoGame(){
+    }
+
+    public int[][] makeBoard(){
+        return null;
+    }
+
+    public int chooseRandomNumber(){
+        return 0;
+    }
+
+    public int checkFiveInARow(){
+        return 0;
+    }
+
+    public int[][] updateBoard(){
+        return null;
+    }
+
+    public String formatChosenNumber(){
+        return null;
+    }
+
+    public int freeSpace(){
+        return 0;
+    }
+
+    public String outputBoard(){
+        return null;
+    }
 }
