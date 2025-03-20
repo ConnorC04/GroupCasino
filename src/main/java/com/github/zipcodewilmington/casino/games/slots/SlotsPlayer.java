5e6af1316c7288a5176061215f4baed1ec843046
GabrielC67 <gabecruzpro@gmail.com>
2025-03-20 16:16:28 -0400

    SlotsPlayer draft done and inspected by master Repo holder. Once implementation takes place, the code will reflect the changes and linking to Casino and SlotsGame.

--- src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsPlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsPlayer.java
@@ -1,5 +1,5 @@
 package com.github.zipcodewilmington.casino.games.slots;
-
+import  java.util.Scanner;
 /**
  * Created by leon on 7/21/2020.
  */
@@ -13,5 +13,21 @@ public class SlotsPlayer {
     "Are you ready? Pull the trigger to begin!!!" --> SlotsGame class becomes active
     */
 
+    public void player(){
+        //Scanner scanner = new Scanner(System.in);
+        //double walletBalance = wallet linked to player account.
+        //double moneyToPLaySlots = walletBalance - moneyFromWallet(walletBalance);
+        //Transfer to SlotsGame.java
+    }
 
+    public String getUserInput(String s){
+        System.out.println(s);
+        Scanner addMoney = new Scanner(System.in);
+        return addMoney.nextLine();
+    }
+
+    public double moneyFromWallet(/*double walletaccountfromPlayerAfterEnteringfromCasino*/){
+        double moneyFromCasinoPlayerWallet = Double.parseDouble(getUserInput("Hi! Welcome to SLOTS! How much money do you want to play for SLOTS?"));
+        return (moneyFromCasinoPlayerWallet);
+    }
 }
