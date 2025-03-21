0d289f4dd120336747ecbaa53da3caabe4debc05
ConnorC04 <connorjcasey@iCloud.com>
2025-03-21 14:06:49 -0400

    Bingo before Beauty

--- src/main/java/com/github/zipcodewilmington/casino/games/bingo/BingoPlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/bingo/BingoPlayer.java
@@ -1,4 +1,48 @@
 package com.github.zipcodewilmington.casino.games.bingo;
 
-public class BingoPlayer {
+import com.github.zipcodewilmington.casino.CasinoAccount;
+import com.github.zipcodewilmington.casino.PlayerInterface;
+
+
+public class BingoPlayer implements PlayerInterface {
+    Double wallet = 1000000.0;
+    String accountName ;
+    String accountPassword;
+    public BingoPlayer(){
+
+    }
+
+    public BingoPlayer(String accountName, String accountPassword){
+        this.wallet=wallet;
+        this.accountName=accountName;
+        this.accountPassword=accountPassword;
+    }
+    @Override
+    public CasinoAccount getArcadeAccount() {
+        return null;
+    }
+
+    @Override
+    public String getAccountName() {
+        return this.accountName;
+
+    }
+
+    @Override
+    public String getAccountPassword() {
+        return this.accountPassword;
+
+    }
+
+    @Override
+    public Double getAccountBalance() {
+        return this.wallet;
+
+    }
+
+    @Override
+    public <SomeReturnType> SomeReturnType play() {
+        return null;
+    }
+
 }
