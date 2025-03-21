e67ed7ab7a3323c430f12460950b673acb163f6c
Kanika <kanika@Zipcoders-MacBook-Pro-3.local>
2025-03-21 16:23:11 -0400

    2 test running

--- src/test/java/com/github/zipcodewilmington/WarTest.java
+++ src/test/java/com/github/zipcodewilmington/WarTest.java
@@ -14,7 +14,7 @@ public void testcreatedDeck() {
     //given
     WarGame warGame = new WarGame();
     //when
-    List<Integer> deck = game.createdDeck();
+    List<Integer> deck = warGame.createdDeck();
     //then
     assertEquals(52, deck.size());
 }
@@ -24,8 +24,8 @@ public void testCreatedShuffle() {
 
     //given
     WarGame warGame = new WarGame();
-    List<Integer> deck1 = game.createdDeck();
-    List<Integer> deck2 = game.createdDeck();
+    List<Integer> deck1 = warGame.createdDeck();
+    List<Integer> deck2 = warGame.createdDeck();
 
     //when
     boolean areEqual = deck1.equals(deck2);
